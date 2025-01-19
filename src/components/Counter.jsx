100a87d71a1985323c28dc3d78e228c467fcf6d9
arshad012 <forarshad99@gmail.com>
2025-01-19 11:10:42 +0530

    added div nth child 3

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrease } from "../redux/counterSlice";
+import '../styles/App.css';
 
 const Counter = () => {
     const {count} = useSelector(state => state.counter);
@@ -8,13 +9,12 @@ const Counter = () => {
 
     return (
         <div>
-            <h1 style={{textAlign: 'center'}}>{count}</h1>
-            <div className="d-flex p-2 justify-content-center gap-3">
-                <button type="button" onClick={() => dispatch(decrease(1))} className="btn btn-outline-danger decrease">decrease</button>
-                <button type="button" onClick={() => dispatch(increment(1))} className="btn btn-success increment">increment</button>
-            </div>
+            <h1>{count}</h1>
+            <button type="button" onClick={() => dispatch(decrease(1))} className="btn btn-outline-danger decrease">decrease</button>
+            <button type="button" onClick={() => dispatch(increment(1))} className="btn btn-success increment">increment</button>
         </div>
     )
 }
 
 export default Counter;
+{/* <div className="d-flex p-2 justify-content-center gap-3"></div> */}
